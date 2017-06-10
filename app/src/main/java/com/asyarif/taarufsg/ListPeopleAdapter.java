0dccd2b60942bc0f7d2af35013caca210d24db11
Ahmad Syarif <ahmadsyarif92@gmail.com>
2017-06-10 23:51:54 +0800

    add firebase service

--- app/src/main/java/com/asyarif/taarufsg/ListPeopleAdapter.java
+++ app/src/main/java/com/asyarif/taarufsg/ListPeopleAdapter.java
@@ -2,6 +2,7 @@ package com.asyarif.taarufsg;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +10,87 @@ import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 
 /**
  * Created by asyarif on 10/06/17.
  */
 
 public class ListPeopleAdapter extends RecyclerView.Adapter<ListPeopleAdapter.ListPeopleViewHolder> {
+
+    private static final String TAG = ListPeopleAdapter.class.getSimpleName();
+
+    private DatabaseReference mDatabase;
+    private DatabaseReference mDatabaseRoot;
+    private ArrayList<User> mUsers;
+
+    private int mNumberOfUser;
+
+    public ListPeopleAdapter(){
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mDatabaseRoot  = mDatabase.child("database");
+
+        mUsers = new ArrayList<User>();
+        mUsers.clear();
+
+        mDatabaseRoot.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                mNumberOfUser = (int)dataSnapshot.getChildrenCount();
+
+                for(DataSnapshot user : dataSnapshot.getChildren()){
+                    String name = (String)user.child("name").getValue();
+                    String birthday = (String)user.child("birthday").getValue();
+                    String description = (String)user.child("description").getValue();
+
+                    mUsers.add(new User(name,birthday,description));
+                }
+
+                notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "onCancelled", databaseError.toException());
+            }
+        });
+
+        mDatabaseRoot.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get Post object and use the values to update the UI
+                mNumberOfUser = (int)dataSnapshot.getChildrenCount();
+                mUsers.clear();
+                for(DataSnapshot user : dataSnapshot.getChildren()){
+                    String name = (String)user.child("name").getValue();
+                    String birthday = (String)user.child("birthday").getValue();
+                    String description = (String)user.child("description").getValue();
+
+                    mUsers.add(new User(name,birthday,description));
+                }
+
+                notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "onCancelled", databaseError.toException());
+            }
+        });
+    }
+
+
 
 
     @Override
@@ -40,7 +116,7 @@ public class ListPeopleAdapter extends RecyclerView.Adapter<ListPeopleAdapter.Li
 
     @Override
     public int getItemCount() {
-        return 3;
+        return mUsers.size();
     }
 
     class ListPeopleViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -69,27 +145,20 @@ public class ListPeopleAdapter extends RecyclerView.Adapter<ListPeopleAdapter.Li
         }
 
         public void bind(int position) {
-            if(position==0){
-                mUser.setText("Syarif");
+
+            try{
+                User user = mUsers.get(position);
+
+                mUser.setText(user.mName);
                 mMaleIcon.setVisibility(View.VISIBLE);
                 mFemaleIcon.setVisibility(View.INVISIBLE);
-                mBirthday.setText("20 April 1992");
-                mDescription.setText("Hi, My name is syarif and I'm married already. so sorry loh");
+                mBirthday.setText(user.mBirthday);
+                mDescription.setText(user.mDescription);}
+            catch (IndexOutOfBoundsException e)
+            {
+                Log.v(TAG,e.getMessage());
             }
-            else if (position==1){
-                mUser.setText("Multazam");
-                mMaleIcon.setVisibility(View.VISIBLE);
-                mFemaleIcon.setVisibility(View.INVISIBLE);
-                mBirthday.setText("21 April 1992");
-                mDescription.setText("Hi, My name is Multazam and I'm not married yet. so come on loh");
-            }
-            else if (position==2){
-                mUser.setText("Aziz");
-                mMaleIcon.setVisibility(View.VISIBLE);
-                mFemaleIcon.setVisibility(View.INVISIBLE);
-                mBirthday.setText("31 Mei 1992");
-                mDescription.setText("Hi, My name is Aziz and I'm not married yet. so come on loh");
-            }
+
         }
     }
 }
---
+++ app/src/main/java/com/asyarif/taarufsg/User.java
@@ -0,0 +1,17 @@
+package com.asyarif.taarufsg;
+
+/**
+ * Created by asyarif on 10/06/17.
+ */
+
+public class User {
+    public String mName;
+    public String mBirthday;
+    public String mDescription;
+
+    public User(String name, String birthday, String description){
+        mName=name;
+        mBirthday = birthday;
+        mDescription = description;
+    }
+}
